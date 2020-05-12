20d7964fec8076c592a49c5d902b8b2dc9b33174
민진 <whxorb44@gmail.com>
2020-05-12 23:46:07 +0900

    20200512

---
+++ 프로그래머스/브라이언의 고민.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <map>
+#include <set>
+#include <queue>
+
+using namespace std;
+typedef long long ll;
+
+string solution(string sentence) {
+	string answer = "";
+	return answer;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	return 0;
+}
