5aaa121268ef9c992539b763fb41a07a53f6e305
민진 <whxorb44@gmail.com>
2020-06-11 04:14:42 +0900

    20200610

--- BOJ/14864_줄서기.cpp
+++ BOJ/14864_줄서기.cpp
@@ -13,15 +13,43 @@ using namespace std;
 
 typedef long long ll;
 
-void solve()
-{
-
-}
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-	solve();
-	cout <<   '\n';
+	int n, m; cin >> n >> m;
+	
+	vector<int> v(n + 1);
+	for (int i = 1; i <= n; i++)
+		v[i] = i;
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b; cin >> a >> b;
+		v[a]++;
+		v[b]--;
+	}
+
+	bool flg = true;
+	vector<bool> visit(n + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		if (visit[v[i]])
+		{
+			flg = false;
+			break;
+		}
+		visit[v[i]] = true;
+	}
+
+	if (flg)
+	{
+		for (int i = 1; i <= n; i++)
+			cout << v[i] << ' ';
+		cout << '\n';
+	}
+	else
+		cout << "-1\n";
+
 	return 0;
 }
